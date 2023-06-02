3730ee5aa164d34e35f1a3620e58e612d148351d
JacobLan <maimaisatt@gmail.com>
2023-06-03 00:16:29 +0800

    TreatmentController開始

--- ReproductiveLab_Service/Services/TreatmentService.cs
+++ ReproductiveLab_Service/Services/TreatmentService.cs
@@ -1,11 +1,15 @@
-﻿using ReproductiveLab_Common.Dtos;
+﻿using Microsoft.EntityFrameworkCore;
+using ReproductiveLab_Common.Dtos;
+using ReproductiveLab_Common.Dtos.ForTreatment;
 using ReproductiveLab_Repository.Interfaces;
 using ReproductiveLab_Service.Interfaces;
+using ReproductiveLabDB.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace ReproductiveLab_Service.Services
 {
@@ -25,6 +29,66 @@ namespace ReproductiveLab_Service.Services
                 i.treatmentDay = $"D{treatmentDay.Days}";
             }
             return result;
+        }
+        public BaseResponseDto AddOvumPickupNote(AddOvumPickupNoteDto ovumPickupNote)
+        {
+            BaseResponseDto result = new BaseResponseDto();
+            string errorMessage = OvumPickupNoteValidation(ovumPickupNote);
+            if (string.IsNullOrEmpty(errorMessage))
+            {
+                try
+                {
+                    using (TransactionScope scope = new TransactionScope())
+                    {
+                        OvumPickup ovumPickup = new OvumPickup()
+                        {
+                            TriggerTime = (DateTime)ovumPickupNote.operationTime.triggerTime,
+                            StartTime = (DateTime)ovumPickupNote.operationTime.startTime,
+                            EndTime = (DateTime)ovumPickupNote.operationTime.endTime,
+                            CocGrade5 = ovumPickupNote.ovumPickupNumber.coc_Grade5,
+                            CocGrade4 = ovumPickupNote.ovumPickupNumber.coc_Grade4,
+                            CocGrade3 = ovumPickupNote.ovumPickupNumber.coc_Grade3,
+                            CocGrade2 = ovumPickupNote.ovumPickupNumber.coc_Grade2,
+                            CocGrade1 = ovumPickupNote.ovumPickupNumber.coc_Grade1,
+                            Embryologist = Guid.Parse(ovumPickupNote.embryologist),
+                            UpdateTime = DateTime.Now
+                        };
+                        sharedFunction.SetMediumInUse<OvumPickup>(ovumPickup, ovumPickupNote.mediumInUse);
+                        dbContext.OvumPickups.Add(ovumPickup);
+                        dbContext.SaveChanges();
+                        Guid latestOvumPickupId = dbContext.OvumPickups.OrderByDescending(x => x.SqlId).Select(x => x.OvumPickupId).FirstOrDefault();
+                        int ovumTotalNumber = ovumPickupNote.ovumPickupNumber.coc_Grade1 + ovumPickupNote.ovumPickupNumber.coc_Grade2 + ovumPickupNote.ovumPickupNumber.coc_Grade3 + ovumPickupNote.ovumPickupNumber.coc_Grade4 + ovumPickupNote.ovumPickupNumber.coc_Grade5;
+                        for (int i = 1; i <= ovumTotalNumber; i++)
+                        {
+                            OvumDetail ovumDetail = new OvumDetail()
+                            {
+                                CourseOfTreatmentId = ovumPickupNote.courseOfTreatmentId,
+                                OvumFromCourseOfTreatmentId = ovumPickupNote.courseOfTreatmentId,
+                                OvumPickupId = latestOvumPickupId,
+                                OvumNumber = i,
+                                OvumDetailStatusId = (int)OvumDetailStatusEnum.Incubation
+                            };
+                            dbContext.Add(ovumDetail);
+                        }
+                        dbContext.SaveChanges();
+                        scope.Complete();
+
+                    }
+                    result.SetSuccess();
+                }
+                catch (TransactionAbortedException ex)
+                {
+                    result.SetError(ex.Message);
+                }
+            }
+            else
+            {
+                result.SetError(errorMessage);
+            }
+            return result;
+
+
+
         }
     }
 }
